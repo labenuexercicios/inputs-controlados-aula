e86aba8049820f2d85c0613650d1e9e0dad3148e
guirozmesquita@gmail.com <guilhermesquita>
2022-10-18 09:08:24 -0400

    template finalizado

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,24 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {MainContainer, Form, Input} from './styles'
 
 function MainPage() {
+  
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [mail, setMail] = useState('');
+  
+  function handleName(e){
+    setName(e.target.value)
+    console.log(e.target.value);
+  }
+
+  function handleAge(e){
+    setAge(e.target.value)
+    console.log(e.target.value)
+  }
+
+  function handleMail(e){
+    setMail(e.target.value)
+    console.log(e.target.value)
+  }
+
+
+  const clear = (e)=>{
+    let data = {
+      name: name,
+      age: Number(age),
+      email: mail
+    }
+    console.log(data);
+
+    setName('');
+    setAge('');
+    setMail('')
+  }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
       <Form>
         <label>
           Nome:
-          <Input />
+          <Input onChange={handleName} value={name}/>
         </label>
         <label>
           Idade:
-          <Input/>
+          <Input onChange={handleAge} value={age}/>
         </label>
         <label>
           E-mail:
-          <Input />
+          <Input onChange={handleMail} value={mail}/>
         </label>
-      <button>Enviar dados</button>
+      <button onClick={clear}>Enviar dados</button>
       </Form>
     </MainContainer>
   )
