56e65a29455e365eaa09fcfe4094d46f0eed1287
carolinabispo <carolinabispos07@gmail.com>
2023-05-24 22:09:59 -0300

    exercicios sobre input controlado

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,23 +1,72 @@
-import React from 'react'
-import { MainContainer, Form, Input } from './styles'
+import React, {useState} from "react";
+import { MainContainer, Form, Input } from "./styles";
 
 function MainPage() {
+  const [nameInput, setNameInput] = useState('')
+  const [ageInput, setAgeInput] = useState('')
+  const [civilState, setCivilState] = useState('')
+  const [password, setPassord] = useState('')
+  const [email, setEmail] = useState('')
+
+    // const handleChange = (e) =>{
+    //   setNameInput(e.target.value)
+    // }
+
+    // const handleChange2 = (e) =>{
+    //   setAgeInput(e.target.value)
+    // }
+
+    const handleChange = (e, setState) =>{
+      setState(e.target.value)
+    }
+
+
+    const inputClear = (e) =>{
+      e.preventDefault()
+      return [setAgeInput(''), setCivilState(''), setEmail(''), setPassord(''), setNameInput('')]
+      // if(nameInput.length && ageInput.length && civilState.length){
+      //   console.log(nameInput, ageInput, civilState);
+      // } else {
+      //   alert('Preencha os campos abaixo')
+      // }
+    }
+
   return (
     <MainContainer>
       <h2>Formulário de inscrição</h2>
-      <Form>
+      <Form onSubmit = {inputClear}>
         <label>
           Nome:
-          <Input />
+          <Input value={nameInput} onChange ={(e) => handleChange(e,setNameInput)}/>
         </label>
         <label>
           Idade:
-          <Input />
+          <Input value = {ageInput} onChange={(e) => handleChange(e, setAgeInput)} />
         </label>
+
+        <label >
+          Email
+          <Input value={email} onChange={(e) => handleChange(e, setEmail)} type={'email'} />
+        </label>
+
+        <label>
+          Senha 
+          <Input value={password} onChange={(e) => handleChange(e,setPassord)} type={'password'} />
+        </label>
+
+        <select value={civilState} onChange={(e) => handleChange(e,setCivilState)}>
+
+        <option value="vazio">Selecione as opções abaixo</option>
+        <option value="Nenhum">Nenhum</option>
+          <option value="Solteiro">Solteiro (a)</option>
+          <option value="Casado">Casado (a)</option>
+          <option value="Viuvo">Viuvo (a)</option>
+          <option value="Divorciado">Divorciado (a)</option>
+        </select>
         <button>Enviar dados</button>
       </Form>
     </MainContainer>
-  )
+  );
 }
 
-export default MainPage
+export default MainPage;
